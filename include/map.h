bc16baa77fd30f7179daeeef9077760462072689
Nathan Trugilho Braga <trugicraft@gmail.com>
2025-10-10 16:11:46 -0300

    Defini as estruturas

--- include/map.h
+++ include/map.h
@@ -3,6 +3,21 @@
 
 #include <GL/glut.h>
 
+#define MAP_SIZE 100
+
+typedef struct vertex {
+    float x;
+    float y;
+    float z;
+} vertex;
+
+typedef struct square {
+    vertex A;
+    vertex B;
+    vertex C;
+    vertex D;
+} square;
+
 void drawMap();
 
 #endif
